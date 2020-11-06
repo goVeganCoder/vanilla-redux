d474ab55a87fa8ea0a07fbe2a640645094998410
modori <mintsatang2@naver.com>
2020-11-06 09:15:03 +0900

    counter done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number= document.querySelector('span');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+//reducer는 data를 변경하는 함!수! return 하는게 데이터가 됨
+//action으로 modify하게 함
+const countModifier = (count=0, action)=>{
+switch (action.type){
+  case ADD :
+    return count +1;
+  case MINUS :
+    return count -1;
+  default:
+    return count;
+}
+  
+};
+
+//redux는 reducer를 parameter로 원함
+const cousntStore = createStore(countModifier);
+
+const onChange = () =>{
+  console.log('change');
+  number.innerText = cousntStore.getState();
+}
+
+cousntStore.subscribe(onChange);
+//dispatch 로 action을 보낼 수 있음
+
+
+const handleMinus = () =>{
+  cousntStore.dispatch({type:MINUS})
+}
+add.addEventListener('click', () => cousntStore.dispatch({type:ADD}));
+minus.addEventListener('click', handleMinus);
